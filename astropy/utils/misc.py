feec3b13e4f063f78eb913cd16ac6622539391e0
tbhat-ops <tbhat@ucsd.edu>
2026-03-08 16:35:58 -0700

    Solution submission for astropy-7166 at 2026-03-08T16:35:58.737427

--- astropy/utils/misc.py
+++ astropy/utils/misc.py
@@ -535,8 +535,7 @@ class InheritDocstrings(type):
                     super_method = getattr(base, key, None)
                     if super_method is not None:
                         if isinstance(val, property):
-                            if val.fget and val.fget.__doc__ is None:
-                                val.fget.__doc__ = super_method.__doc__
+                            # Always create a new property with the inherited docstring
                             dct[key] = property(val.fget, val.fset, val.fdel, super_method.__doc__)
                         else:
                             val.__doc__ = super_method.__doc__
