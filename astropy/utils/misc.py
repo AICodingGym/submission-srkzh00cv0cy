bb39176fc1d8b748486360ba3d6f4ea5aabd41a9
tbhat-ops <tbhat@ucsd.edu>
2026-03-08 16:01:57 -0700

    Solution submission for astropy-7166 at 2026-03-08T16:01:57.655953

--- astropy/utils/misc.py
+++ astropy/utils/misc.py
@@ -534,7 +534,10 @@ class InheritDocstrings(type):
                 for base in cls.__mro__[1:]:
                     super_method = getattr(base, key, None)
                     if super_method is not None:
-                        val.__doc__ = super_method.__doc__
+                        if isinstance(val, property):
+                            dct[key] = property(val.fget, val.fset, val.fdel, super_method.__doc__)
+                        else:
+                            val.__doc__ = super_method.__doc__
                         break
 
         super().__init__(name, bases, dct)
